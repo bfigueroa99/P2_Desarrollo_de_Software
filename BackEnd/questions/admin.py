cdc81dea0bf1209d876edbbed6d124c5e96f1d7b
Benjamin Figueroa Guzman <bfigueroa@miuandes.cl>
2023-09-11 13:24:45 -0300

    creacion de modelo pregunta y todo lo derivado

--- BackEnd/questions/admin.py
+++ BackEnd/questions/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Pregunta
 
-# Register your models here.
+@admin.register(Pregunta)
+class PreguntaAdmin(admin.ModelAdmin):
+    list_display = ('id', 'tipo', 'nivel_dificultad', 'enunciado')  # Esto es opcional, define qué campos se muestran en la lista de preguntas
--- BackEnd/questions/views.py
+++ BackEnd/questions/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .models import Pregunta
+from .serializers import PreguntaSerializer
+
+class ListaPreguntas(generics.ListCreateAPIView):
+    queryset = Pregunta.objects.all()
+    serializer_class = PreguntaSerializer
+
+class DetallePregunta(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Pregunta.objects.all()
+    serializer_class = PreguntaSerializer
 
 # Create your views here.
