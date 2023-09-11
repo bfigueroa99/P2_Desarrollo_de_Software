3f710d41da185d1f7f705ee364f9a49153b780e7
Benjamin Figueroa Guzman <bfigueroa@miuandes.cl>
2023-09-11 17:04:17 -0300

    pregunta aleatoria

--- BackEnd/questions/urls.py
+++ BackEnd/questions/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('preguntas/', views.ListaPreguntas.as_view(), name='lista_preguntas'),
     path('preguntas/<int:pk>/', views.DetallePregunta.as_view(), name='detalle_pregunta'),
+    path('preguntas/aleatoria/', views.PreguntaAleatoria.as_view(), name='pregunta_aleatoria'),
 ]
--- BackEnd/questions/views.py
+++ BackEnd/questions/views.py
@@ -1,4 +1,7 @@
+import random
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 from rest_framework import generics
 from .models import Pregunta
 from .serializers import PreguntaSerializer
@@ -11,4 +14,13 @@ class DetallePregunta(generics.RetrieveUpdateDestroyAPIView):
     queryset = Pregunta.objects.all()
     serializer_class = PreguntaSerializer
 
-# Create your views here.
+class PreguntaAleatoria(APIView):
+    def get(self, request):
+        # Obtén una pregunta aleatoria de la base de datos
+        preguntas = Pregunta.objects.all()
+        pregunta_aleatoria = random.choice(preguntas)
+
+        # Serializa la pregunta y envía la respuesta
+        serializer = PreguntaSerializer(pregunta_aleatoria)
+        return Response(serializer.data)
+
