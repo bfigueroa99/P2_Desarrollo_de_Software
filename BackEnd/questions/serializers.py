b71ccccae86bf5b51352ed066676b4169725c536
Benjamin Figueroa Guzman <bfigueroa@miuandes.cl>
2023-09-11 14:41:14 -0300

    alternativas

--- BackEnd/questions/serializers.py
+++ BackEnd/questions/serializers.py
@@ -5,3 +5,12 @@ class PreguntaSerializer(serializers.ModelSerializer):
     class Meta:
         model = Pregunta
         fields = '__all__'
+
+    def to_representation(self, instance):
+        data = super().to_representation(instance)
+        if data['tipo'] == 'alternativas':
+            data['alternativa2'] = instance.alternativa2
+            data['alternativa3'] = instance.alternativa3
+            data['alternativa4'] = instance.alternativa4
+        return data
+
