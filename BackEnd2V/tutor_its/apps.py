46e2da5108c38a3552853d4b3379d2da8e2d58fb
Benjamin Figueroa Guzman <bfigueroa@miuandes.cl>
2023-09-21 12:24:34 -0300

    delete: BackEnd2V

--- BackEnd2V/tutor_its/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class TutorItsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'tutor_its'
--- BackEnd2V/tutor_its/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import Pregunta
-
-class PreguntaForm(forms.ModelForm):
-    class Meta:
-        model = Pregunta
-        fields = ['enunciado', 'imagen', 'opciones']
