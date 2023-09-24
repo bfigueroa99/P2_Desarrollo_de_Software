1186b22e5f833b364e4b1917ad871cd21b5e58ff
antowester <awestermeier@miuandes.cl>
2023-09-24 12:57:43 -0300

    Calidad de muestras listo (2)

--- Question/2. Calidad de Mezclas/Calculo Numerico/Facil/png_p1.py
+++ Question/2. Calidad de Mezclas/Calculo Numerico/Facil/png_p1.py
@@ -12,26 +12,38 @@ tamano_letra = 30  # Tamaño de la letra en puntos
 context.set_font_size(tamano_letra)
 context.set_source_rgb(0.0, 0.0, 0.0)  # Negro
 
-#Dibujar el vaso
-context.move_to(100, 400)
-context.line_to(200, 400)
-context.line_to(180, 200)
-context.line_to(120, 200)
+# Dibujar la parte inferior del vaso (con agua)
+context.move_to(200, 400) #Izq abajo
+context.line_to(300, 400) #Der abajo
+context.line_to(335, 250) #Der arriba
+context.line_to(165, 250) #Izq arriba
 context.close_path()  # Cierra el camino
 
-#Rellenar el vaso con un color
-context.set_source_rgb(0.0, 0.0, 1.0)  # Azul
-context.fill_preserve()  # Rellena el vaso
-
-#Contorno del vaso
+# Rellenar la parte inferior del vaso con un color
+context.set_source_rgb(0.7, 0.4, 0.0)  #Naranjo
+context.fill()  # Rellena la parte inferior del vaso
 context.set_source_rgb(0.0, 0.0, 0.0)  # Negro
-context.set_line_width(3)
-context.stroke()
+context.set_line_width(2)  # Ancho de línea
+context.stroke()  # Dibuja la línea
+
+# Dibujar la parte superior del vaso (sin rellenar)
+context.move_to(350, 200)
+context.line_to(335, 250)  
+context.line_to(165, 250) 
+context.line_to(150, 200)
+context.close_path()  # Cierra el camino
+
+# Dibujar una línea en la mitad del vaso (sin rellenar)
+context.set_source_rgb(0.0, 0.0, 0.0)  # Negro
+context.set_line_width(0.5)  # Ancho de línea
+context.stroke()  # Dibuja la línea
 
 context.move_to(10, 580)
-context.show_text(f" Agua = {cantidad_agua_ml:.2f} ml")
+context.show_text(f" Agua = {cantidad_agua_ml} ml")
 context.move_to(10, 620)
-context.show_text(f" Jugo = {cantidad_jugo_naranja_ml:.2f} ml")
+context.show_text(f" Jugo = {cantidad_jugo_naranja_ml} ml")
+
+surface.write_to_png("pds_foto.png")
 
 # Guarda el archivo SVG
 surface.finish()
