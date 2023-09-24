f632d129dc7411084c679a58b498c2cc5099f0db
Benjamin Figueroa Guzman <bfigueroa@miuandes.cl>
2023-09-23 23:03:37 -0300

    si

--- Question/1. Diagrama PVT/Calculo Numerico/Facil/png_p1.py
+++ Question/1. Diagrama PVT/Calculo Numerico/Facil/png_p1.py
@@ -1,6 +1,6 @@
-
 import cairo 
-import random
+import random 
+from calculonumerico2_facil_diagrama_pvt import presion_atm, cambio_de_volumen_litros 
 
 # Create an SVG surface
 surface = cairo.SVGSurface("gas_expansion.svg", 600, 650)
@@ -8,18 +8,18 @@ surface = cairo.SVGSurface("gas_expansion.svg", 600, 650)
 # Add context to the surface
 context = cairo.Context(surface)
 
-# Configurar el tamaño de la letra
-tamano_letra = 50  # Tamaño de la letra en puntos
-context.set_font_size(tamano_letra)
-
 # Configurar el radio de los puntos
-radio_punto = 2
+radio_punto = 3
+
+# Configurar el tamaño de la letra
+tamano_letra = 30  # Tamaño de la letra en puntos
+context.set_font_size(tamano_letra)
 
 #Configurar el color de los puntos
 context.set_source_rgb(0.0, 0.0, 1.0)  #Azul
 
 
-num_puntos = 1000
+num_puntos = 500
 width, height = 600, 500
 for _ in range(num_puntos):
     x = random.uniform(0, width)
@@ -29,12 +29,12 @@ for _ in range(num_puntos):
 
 
 # Add labels for the initial pressure, temperature, and final pressure
-context.move_to(10, 540)
-context.show_text(f"xxxx")
 context.move_to(10, 580)
-context.show_text(f"xxx")
+context.show_text(f"La presión es {presion_atm:.2f}")
+presion_atm, cambio_de_volumen_litros
 context.move_to(10, 620)
-context.show_text(f"xxx")
+context.show_text(f"El cambio de volumen es {cambio_de_volumen_litros:.2f}")
+
 
 surface.write_to_png("pds_foto.png")
 
