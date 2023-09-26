9610b47de1a45709af1a1b07fb8d76504db221ea
raulduha <80332171+raulduha@users.noreply.github.com>
2023-09-26 08:34:36 -0300

    crud inicial(falta) y vista de alumnos por parte de profesor

--- FrontEnd/front_p2/src/components/AccountPage/AccountPage.js
+++ FrontEnd/front_p2/src/components/AccountPage/AccountPage.js
@@ -1,22 +1,15 @@
 import React, { useState, useEffect } from 'react';
 import './AccountPage.css';
 import { useAuth } from '../../auth/AuthProvider'; // Importa tu contexto de autenticación aquí
-import { ref, get, database } from 'firebase/database';
+import { ref, get } from 'firebase/database';
 import { getDatabase } from 'firebase/database';
+import axios from 'axios';
 
 function AccountPage() {
   const { user } = useAuth();
-
-  // Simulación de datos de progreso del curso
-  const courseProgress = [
-    { unit: 'Unidad 1', progress: 80 },
-    { unit: 'Unidad 2', progress: 60 },
-    { unit: 'Unidad 3', progress: 90 },
-    { unit: 'Unidad 4', progress: 75 },
-    { unit: 'Unidad 5', progress: 50 },
-  ];
-
-  // Obtener el nivel y puntaje del usuario desde Firebase Realtime Database
+  const [userRole, setUserRole] = useState(null);
+  const [alumnos, setAlumnos] = useState([]);
+  const [preguntas, setPreguntas] = useState([]);
   const [userProgress, setUserProgress] = useState({ nivel: 0, puntaje: 0 });
 
   useEffect(() => {
@@ -26,15 +19,67 @@ function AccountPage() {
       
       get(userRef).then((snapshot) => {
         if (snapshot.exists()) {
-          setUserProgress(snapshot.val());
+          setUserRole(snapshot.val().rol);
+          setUserProgress(snapshot.val()); // Actualizar el progreso del usuario
         } else {
-          console.log('El usuario no tiene datos de progreso en la base de datos.');
+          console.log('El usuario no tiene datos de rol en la base de datos.');
         }
       }).catch((error) => {
-        console.error('Error al obtener datos de progreso:', error);
+        console.error('Error al obtener datos de rol:', error);
       });
+
+      // Consultar la lista de alumnos si el usuario es profesor
+      if (userRole === 'profesor') {
+        const alumnosRef = ref(db, 'usuarios');
+        get(alumnosRef)
+          .then((snapshot) => {
+            if (snapshot.exists()) {
+              // Filtrar solo a los alumnos (rol: 'alumno')
+              const alumnosList = [];
+              snapshot.forEach((childSnapshot) => {
+                const userData = childSnapshot.val();
+                if (userData.rol === 'alumno') {
+                  alumnosList.push(userData);
+                }
+              });
+              setAlumnos(alumnosList);
+            } else {
+              console.log('No hay datos de alumnos en la base de datos.');
+            }
+          })
+          .catch((error) => {
+            console.error('Error al obtener datos de alumnos:', error);
+          });
+
+        // Consultar las preguntas a tu API de Django
+        axios.get('http://143.198.98.190:8000/preguntas/')
+          .then((response) => {
+            setPreguntas(response.data);
+          })
+          .catch((error) => {
+            console.error('Error al obtener preguntas:', error);
+          });
+      }
     }
-  }, [user]);
+  }, [user, userRole]);
+
+  // Función para editar una pregunta
+  const editarPregunta = (preguntaId) => {
+    // Implementa la lógica para editar una pregunta aquí
+    console.log('Editar pregunta con ID:', preguntaId);
+  };
+
+  // Función para eliminar una pregunta
+  const eliminarPregunta = (preguntaId) => {
+    // Implementa la lógica para eliminar una pregunta aquí
+    console.log('Eliminar pregunta con ID:', preguntaId);
+  };
+
+  // Función para crear una nueva pregunta
+  const crearPregunta = () => {
+    // Implementa la lógica para crear una pregunta aquí
+    console.log('Crear nueva pregunta');
+  };
 
   return (
     <div className="account-page">
@@ -62,7 +107,36 @@ function AccountPage() {
         </div>
       </div>
 
-      {/* Resto del componente */}
+      {/* Mostrar la lista de alumnos si el usuario es profesor */}
+      {userRole === 'profesor' && (
+        <div>
+          <h2>Lista de Alumnos</h2>
+          <ul>
+            {alumnos.map((alumno) => (
+              <li key={alumno.uid}>
+                {alumno.nombre} - Nivel: {alumno.nivel}
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
+
+      {/* Mostrar las preguntas si el usuario es profesor */}
+      {userRole === 'profesor' && (
+        <div>
+          <h2>Lista de Preguntas</h2>
+          <ul>
+            {preguntas.map((pregunta) => (
+              <li key={pregunta.id}>
+                {pregunta.texto}
+                <button onClick={() => editarPregunta(pregunta.id)}>Editar</button>
+                <button onClick={() => eliminarPregunta(pregunta.id)}>Eliminar</button>
+              </li>
+            ))}
+          </ul>
+          <button onClick={crearPregunta}>Crear Pregunta</button>
+        </div>
+      )}
     </div>
   );
 }
